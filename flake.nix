5fe5e0b33ad9984c51f2ccfc99fd6a835c7b08dd
Amy de Buitléir <amy@nualeargais.ie>
2022-03-12 00:10:10 +0000

    initial commit

--- flake.nix
+++ flake.nix
@@ -1,62 +1,69 @@
 {
-  description = "A greeting that uses Haskell and flakes.";
+  description = "A greeting implemented in Haskell + a Nix flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        haskellPackages = pkgs.haskellPackages;
-        packageName = "hello-haskell-flake";
-      in
-        rec {
-          packages.${packageName} =
-            with haskellPackages; with pkgs;
-              mkDerivation {
-                pname = packageName;
-                version = "0.1.0.0";
-                src = ./.;
-                isLibrary = false;
-                isExecutable = true;
-                buildDepends = [ makeWrapper ];
-                executableHaskellDepends = [ base directory process random ];
-                license = "unknown";
-                hydraPlatforms = lib.platforms.none;
-              };
+  outputs = { self, nixpkgs, utils, ... }:
+    let
+      package = { haskellPackages, stdenvNoCC }:
+        stdenvNoCC.mkDerivation {
+          name = "hello-haskell-flake";
 
-          # # A Nixpkgs overlay.
-          # overlay = final: prev: {
-          #   hello = with final; stdenv.mkDerivation rec {
-          #     name = "hello-${version}";
-          #     src = ./.;
-          #     nativeBuildInputs = [ autoreconfHook ];
-          #   };
-          # };
+          src = ./.;
+          buildInputs = [
+            (haskellPackages.ghcWithPackages
+              (ps: with ps; [ ]))
+          ];
 
-          defaultPackage = self.packages.${system}.${packageName};
+          buildPhase = "ghc -Wall app/Main.hs";
+          installPhase = "install -D app/Main $out/bin/hello-haskell-flake";
+        };
 
-          # A NixOS module, if applicable (e.g. if the package provides a system service).
-          nixosModules.${packageName} =
-            { pkgs, ... }:
-            {
-              nixpkgs.overlays = [ self.overlay ];
+      module = { pkgs, config, lib, ... }:
+        with lib;
+        let
+          cfg = config.services.hello-haskell-flake;
+          hello-haskell-flake = "${self.packages.${pkgs.system}.hello-haskell-flake}/bin/hello-haskell-flake";
+        in {
+          options.services.hello-haskell-flake = {
+            enable = mkEnableOption "Automatic fan controller";
 
-              environment.systemPackages = [ packages.${packageName} ];
-
-              #systemd.services = { ... };
+            location = mkOption {
+              description = "Location to fetch temperature data for.";
+              type = types.str;
             };
-        
-          devShell = pkgs.mkShell {
-            buildInputs = with haskellPackages;
-              [ ghc
-                haskell-language-server
-                cabal-install
-              ];
-            inputsFrom = builtins.attrValues self.packages.${system};
           };
-        }
-    );
+
+          config.systemd.services.hello-haskell-flake = {
+            description = "Automatic fan controller";
+
+            wants = [ "network-online.target" ];
+            after = [ "network-online.target" ];
+            wantedBy = [ "default.target" ];
+
+            serviceConfig = {
+              Restart = "on-failure";
+              ExecStart = "${hello-haskell-flake} ${escapeShellArg cfg.location}";
+            };
+
+            # The Haskell program assumes that the fan is off when it starts
+            preStart = "echo 1-1 >/sys/bus/usb/drivers/usb/unbind || exit 0";
+            postStop = "echo 1-1 >/sys/bus/usb/drivers/usb/unbind || exit 0";
+          };
+        };
+
+    in {
+      nixosModules.hello-haskell-flake = module;
+    } // (utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          hello-haskell-flake = pkgs.callPackage package { };
+        in {
+          packages.hello-haskell-flake = hello-haskell-flake;
+          defaultPackage = hello-haskell-flake;
+        }));
 }
