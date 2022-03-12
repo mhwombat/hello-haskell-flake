ab84613056c2775baf4b7d1a41b4249d9d13c86d
Amy de Buitléir <amy@nualeargais.ie>
2022-03-12 00:24:17 +0000

    initial commit

--- flake.nix
+++ flake.nix
@@ -23,36 +23,10 @@
         };
 
       module = { pkgs, config, lib, ... }:
-        with lib;
-        let
-          cfg = config.services.hello-haskell-flake;
-          hello-haskell-flake = "${self.packages.${pkgs.system}.hello-haskell-flake}/bin/hello-haskell-flake";
-        in {
-          options.services.hello-haskell-flake = {
-            enable = mkEnableOption "Automatic fan controller";
-
-            location = mkOption {
-              description = "Location to fetch temperature data for.";
-              type = types.str;
-            };
-          };
-
-          config.systemd.services.hello-haskell-flake = {
-            description = "Automatic fan controller";
-
-            wants = [ "network-online.target" ];
-            after = [ "network-online.target" ];
-            wantedBy = [ "default.target" ];
-
-            serviceConfig = {
-              Restart = "on-failure";
-              ExecStart = "${hello-haskell-flake} ${escapeShellArg cfg.location}";
-            };
-
-            # The Haskell program assumes that the fan is off when it starts
-            preStart = "echo 1-1 >/sys/bus/usb/drivers/usb/unbind || exit 0";
-            postStop = "echo 1-1 >/sys/bus/usb/drivers/usb/unbind || exit 0";
-          };
+        {
+          nixpkgs.overlays = [ self.overlay ];
+          environment.systemPackages = [ pkgs.hello-haskell-flake ];
+          #systemd.services = { ... };
         };
 
     in {
