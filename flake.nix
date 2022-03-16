e41c0c5d35a35d4350f68f5f2efc89a8a84ba047
Amy de Buitléir <amy@nualeargais.ie>
2022-03-16 13:11:30 +0000

    initial commit

--- flake.nix
+++ flake.nix
@@ -17,14 +17,47 @@
           pkgs.haskell.lib.doJailbreak (pkg.overrideAttrs (_: { meta = { }; }));
 
         packageName = "hello-haskell-flake";
+        versionNum = "0.1.0.0";
       in {
-        packages.${packageName} = # (ref:haskell-package-def)
-          haskellPackages.callCabal2nix packageName self rec {
-            # Dependency overrides go here
-          };
+        packages.${packageName} = pkgs.stdenv.mkDerivation rec {
+          pname = "${packageName}";
+          version = "${versionNum}";
+
+          src = ./.;
+
+          nativeBuildInputs = [
+            pkgs.cabal-install
+            pkgs.ghc
+          ];
+
+          unpackPhase =
+            ''
+              cp -r $src/* .
+            '';
+
+          configurePhase =
+            ''
+              mkdir -p cabal
+              CABAL_DIR=$PWD/cabal cabal user-config init
+              sed --in-place '/^repository /d; /^ *url:/d; /^ *--/d' cabal/config
+            '';
+
+          buildPhase =
+            ''
+              CABAL_DIR=$PWD/cabal cabal build
+            '';
+
+          installPhase =
+            ''
+              CABAL_DIR=$PWD/cabal cabal install
+              mkdir -p $out/bin
+              cp cabal/bin/${packageName} $out/bin/
+            '';
+        };
 
         defaultPackage = self.packages.${system}.${packageName};
 
+        # This stuff will be available when you run nix develop, but not nix build.
         devShell = pkgs.mkShell {
           buildInputs = with haskellPackages; [
             haskell-language-server
@@ -34,16 +67,14 @@
           inputsFrom = builtins.attrValues self.packages.${system};
         };
 
-        # # A NixOS module, if applicable (e.g. if the package provides a system service).
-        # nixosModules.${packageName}  =
-        #   { pkgs, ... }:
-        #   {
-        #     nixpkgs.overlays = [ self.overlay ];
-
-        #     environment.systemPackages = [ pkgs.${packageName}  ];
-
-        #     #systemd.services = { ... };
-        #   };
+        # A NixOS module, if applicable (e.g. if the package provides a system service).
+        nixosModules.${packageName}  =
+          { pkgs, ... }:
+          {
+            nixpkgs.overlays = [ self.overlay ];
+            environment.systemPackages = [ pkgs.${packageName}  ];
+            #systemd.services = { ... };
+          };
       });
 }
 
